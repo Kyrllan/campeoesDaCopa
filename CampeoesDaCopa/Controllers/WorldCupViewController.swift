d67381dc02c35f8834179c351dde324ab17d1f3f
Kyrllan Nogueira <kyrllan@hotmail.com>
2023-09-23 22:32:47 -0300

    upload project

---
+++ CampeoesDaCopa/Controllers/WorldCupViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WorldCupViewController.swift
+//  CampeoesDaCopa
+//
+//  Created by Kyrllan Nogueira on 02/06/19.
+//  Copyright © 2019 Kyrllan Nogueira. All rights reserved.
+//
+
+import UIKit
+
+class WorldCupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ CampeoesDaCopa/Models/WorldCup.swift
@@ -0,0 +1,34 @@
+//
+//  WorldCup.swift
+//  CampeoesDaCopa
+//
+//  Created by Kyrllan Nogueira on 02/06/19.
+//  Copyright © 2019 Kyrllan Nogueira. All rights reserved.
+//
+
+import Foundation
+
+
+struct WorldCup: Codable {
+    let year: Int
+    let country: String
+    let winner: String
+    let vice: String
+    let winnerScore: String
+    let viceScore: String
+    let matches: [Match]
+}
+
+
+struct Match: Codable {
+    let stage: String
+    let games: [Game]
+}
+
+
+struct Game: Codable {
+    let home: String
+    let away: String
+    let score: String
+    let date: String
+}
